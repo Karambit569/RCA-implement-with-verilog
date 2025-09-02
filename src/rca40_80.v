72dfdee06c0c8da354772ae329bded0ff2764188
Karambit569 <diggertv569@gmail.com>
2025-09-02 20:30:24 +0900

    Update rca40_80.v

--- src/rca40_80.v
+++ src/rca40_80.v
@@ -1,6 +1,6 @@
-// 80비트 RCA - 40비트 RCA 2개로 구성
-// 입력: 80비트 A, B, 1비트 Cin
-// 출력: 80비트 합 S, 최종 캐리 Cout
+// 80-bit RCA - constructed using two 40-bit RCAs
+// Input: 80-bit A, B, 1-bit Cin
+// Output: 80-bit sum S, final carry Cout
 module rca_80b_40 (S, A, B, Cout, Cin);
     input [79:0] A, B;
     input Cin;
@@ -9,7 +9,7 @@ module rca_80b_40 (S, A, B, Cout, Cin);
 
     wire carry;
 
-    // 40비트 RCA 2개로 80비트 연산 구성
+    // Connect two 40-bit RCAs to build an 80-bit adder
     rca_40b rca0 (S[39:0],  A[39:0],  B[39:0],  carry, Cin);
     rca_40b rca1 (S[79:40], A[79:40], B[79:40], Cout, carry);
 endmodule
