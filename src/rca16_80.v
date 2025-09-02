8924b0642746cfa893b75e375b064ca1f19091ea
Karambit569 <diggertv569@gmail.com>
2025-09-02 20:29:28 +0900

    Update rca16_80.v

--- src/rca16_80.v
+++ src/rca16_80.v
@@ -1,6 +1,6 @@
-// 80비트 RCA - 16비트 RCA 5개로 구성
-// 입력: 80비트 A, B, 1비트 Cin
-// 출력: 80비트 합 S, 최종 캐리 Cout
+// 80-bit RCA - constructed using five 16-bit RCAs
+// Input: 80-bit A, B, 1-bit Cin
+// Output: 80-bit sum S, final carry Cout
 module rca_80b_16 (S, A, B, Cout, Cin);
     input [79:0] A, B;
     input Cin;
@@ -9,7 +9,7 @@ module rca_80b_16 (S, A, B, Cout, Cin);
 
     wire [4:0] carry;
 
-    // 16비트 RCA 5개를 연결해 80비트 연산 구성
+    // Connect five 16-bit RCAs to build an 80-bit adder
     rca_16b rca0 (S[15:0],   A[15:0],   B[15:0],   carry[0], Cin);
     rca_16b rca1 (S[31:16],  A[31:16],  B[31:16],  carry[1], carry[0]);
     rca_16b rca2 (S[47:32],  A[47:32],  B[47:32],  carry[2], carry[1]);
